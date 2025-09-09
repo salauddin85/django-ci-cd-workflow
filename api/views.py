149a61dd81f2f46e40eb4e4be7de2ef64cf6138e
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-09-09 17:24:36 +0600

    added pur method in api

--- api/views.py
+++ api/views.py
@@ -16,3 +16,15 @@ class ItemListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        try:
+            item = Item.objects.get(pk=pk)
+        except Item.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ItemSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
